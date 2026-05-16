c22aafa03fd7376ee46ddb49e33d3daf6941b535
Pravash Panigrahi <pravash.cse@gmail.com>
2026-05-16 22:21:27 +0530

    lnkt fix II

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,17 +1,9 @@
 """Package shim exposing root-level producer module as kafka.producer."""
 
-from producer import (
-    BOOTSTRAP_SERVERS,
-    EVENTS_PER_SECOND,
-    TOPIC_RAW,
-    delivery_report,
-    main,
-    make_clickstream_event,
-    make_log_event,
-    make_sensor_event,
-    pick_event,
-    wait_for_kafka,
-)
+from producer import (BOOTSTRAP_SERVERS, EVENTS_PER_SECOND, TOPIC_RAW,
+                      delivery_report, main, make_clickstream_event,
+                      make_log_event, make_sensor_event, pick_event,
+                      wait_for_kafka)
 
 __all__ = [
     "make_clickstream_event",
